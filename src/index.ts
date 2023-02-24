9d9a93ddd071466fdda4f7324140b2cf38088088
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-02-25 04:41:55 +0530

    feat: add google map

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import { Destination } from "./Destination";
+import { Cargo } from "./Cargo";
 
 const destination = new Destination();
 console.log(destination);
+
+const cargo = new Cargo();
+console.log(cargo);
+
+new google.maps.Map(document.getElementById("g-map"), {
+  zoom: 1.6,
+  center: {
+    lat: 0,
+    lng: 20,
+  },
+});
