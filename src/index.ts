9a4c7c0f4b6a0423471adc08c87c54acaa4a0890
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-02-26 11:23:13 +0530

    feat: add tracking point

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Destination } from "./Destination";
 import { Cargo } from "./Cargo";
+import { TrackingMap } from "./TrackingMap";
 
 const destination = new Destination();
 console.log(destination);
@@ -7,10 +8,6 @@ console.log(destination);
 const cargo = new Cargo();
 console.log(cargo);
 
-new google.maps.Map(document.getElementById("g-map"), {
-  zoom: 1.6,
-  center: {
-    lat: 0,
-    lng: 20,
-  },
-});
+const trackingMap = new TrackingMap("g-map");
+trackingMap.attachMarker(destination);
+trackingMap.attachMarker(cargo);
